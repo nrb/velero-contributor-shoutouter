cbf9d42d7734af29a9f4b00ef8d0013d118693b3
Nolan Brubaker <brubakern@vmware.com>
2021-11-17 17:43:47 -0500

    Remove hard-coded lists and values
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"time"
@@ -38,7 +37,7 @@ func main() {
 
 	for _, d := range p.Devs {
 		if d != nil {
-			fmt.Printf("Login: %s, Name: %s\n", d.GetLogin(), d.GetName())
+			fmt.Printf("Login: %s\n", d.GetLogin())
 		}
 	}
 
@@ -117,68 +116,12 @@ func makeConfig() (*ProjectConfig, error) {
 	return c, nil
 }
 
-func getPRs(client *github.Client, repo string) ([]*github.PullRequest, error) {
-	orgName := "vmware-tanzu"
-	prOpts := &github.PullRequestListOptions{State: "closed"}
-	prs, _, err := client.PullRequests.List(context.Background(), orgName, repo, prOpts)
-	if err != nil {
-		return nil, err
-	}
-	return prs, nil
-}
-
-// filterPRsByAuthor returns PRs by authors who were not part of the core maintainers team.
-func filterPRsByAuthor(prs []*github.PullRequest) []*github.PullRequest {
-	authors := []string{
-		"ashish-amarnath",
-		"carlisia",
-		"jonasrosland",
-		"michmike",
-		"nrb",
-		"zubron",
-		"dsu-igeek",
-	}
-
-	var filtered []*github.PullRequest
-
-	for _, pr := range prs {
-		found := false
-		for _, a := range authors {
-			if *pr.User.Login == a {
-				found = true
-			}
-		}
-
-		if !found {
-			filtered = append(filtered, pr)
-		}
-	}
-	return filtered
-}
-
-// filterMergedPRs returns only PRs that have been merged within the last week,
-// not PRs that have been closed without merging.
-func filterMergedPRs(prs []*github.PullRequest) []*github.PullRequest {
-	var filtered []*github.PullRequest
-
-	for _, pr := range prs {
-		if pr.MergedAt != nil {
-			diff := time.Now().Sub(*pr.MergedAt)
-			days := diff.Hours() / 24
-			if days <= 7 {
-				filtered = append(filtered, pr)
-			}
-		}
-
-	}
-	return filtered
-}
-
 // Formats the shoutouts in a consistent format.
 func printShoutout(pr *github.PullRequest) {
 	fmt.Printf("- [@%s](%s): [%s](%s)\n", *pr.User.Login, *pr.User.HTMLURL, *pr.Title, *pr.HTMLURL)
 }
 
+// fromMember determines if a PR was written by a core team member
 func fromMember(pr *github.PullRequest, p *Project) bool {
 	for _, m := range p.Devs {
 		if pr.GetUser().GetLogin() == m.GetLogin() {
